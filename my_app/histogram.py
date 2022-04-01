0377fdc9d880086aafad5a4f6c1fa605dbdff923
jitendrapitchuka <jithendra2900@gmail.com>
2022-04-01 14:36:05 +0530

    changes  in histogram.py

--- my_app/histogram.py
+++ my_app/histogram.py
@@ -2,25 +2,31 @@ import numpy as np
 import cv2
 
 def histogrameq(image):
-    img = cv2.imread(image,0)
+
+
+    img = cv2.imread(image)
+
+    #To display image before equalization
     
 
-   
+    ycrcb=cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
 
 
+    channels=cv2.split(ycrcb)
     a = np.zeros((256,),dtype=np.float16)
 
-    height,width=img.shape
-
+    height,width=channels[0].shape
+    img1=channels[0]
+    
     #finding histogram
     for i in range(width):
         for j in range(height):
-            g = img[j,i]
+            g = channels[0][j,i]
         
             a[g] = a[g]+1
             
 
-   
+
 
 
     #performing histogram equalization
@@ -35,14 +41,24 @@ def histogrameq(image):
     # b now contains the equalized histogram
     b=b.astype(np.uint8)
 
-    
+
 
 
     for i in range(width):
         for j in range(height):
-            g = img[j,i]
-            img[j,i]= b[g]
+            g = channels[0][j,i]
+            channels[0][j,i]= b[g]
+    # channels[0]=img1
+    print(img1)
+    ycrcb=cv2.merge(channels)
+
+    img2=cv2.cvtColor(ycrcb,cv2.COLOR_YCR_CB2BGR)
+    
 
 
-    cv2.imwrite('/home/jitendra/learning/github_repos/django_projects/Image_editor/my_app/media/final.png',img)
+
+
+
+
+    cv2.imwrite('/home/jitendra/learning/github_repos/django_projects/Image_editor/my_app/media/final.png',img2)
     return "/home/jitendra/learning/github_repos/django_projects/Image_editor/my_app/media/final.png"
--- my_app/histogram_rgb.py
+++ my_app/histogram_rgb.py
@@ -9,8 +9,8 @@ cv2.waitKey(0)
 ycrcb=cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
 channels=cv2.split(ycrcb)
 
-img2=cv2.equalizeHist(channels[0],channels[0])
-img2=cv2.merge(channels,ycrcb)
+cv2.equalizeHist(channels[0],channels[0])
+ycrcb=cv2.merge(channels)
 img2=cv2.cvtColor(ycrcb,cv2.COLOR_YCR_CB2BGR,img)
     
 
